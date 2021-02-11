d79ba1b3b3b229b324a8a0e07f9d223ec318839e
gauravkhanna3007 <38395482+gauravkhanna3007@users.noreply.github.com>
2021-02-11 19:42:34 +0530

    first commit

---
+++ Test_for_loop.py
@@ -0,0 +1,10 @@
+marks = 50
+
+if  90<=  marks < 100:
+    print('Grade is A')
+elif 80<= marks <90:
+    print('Grade is B')
+elif 50<= marks <80:
+    print('Grade is C')
+else:
+    print('No Grade')
